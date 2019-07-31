db047a75d88cf5dfd013574d980d292b9b02f669
debian <lucho.2012.tandil@gmail.com>
2019-07-31 16:17:10 -0300

    [DEV] Correccion en pantalla final

--- src/app/pantalla-final/pantalla-final.component.ts
+++ src/app/pantalla-final/pantalla-final.component.ts
@@ -10,7 +10,9 @@ import { PreguntasService } from './../services/preguntas.service';
 })
 export class PantallaFinalComponent implements OnInit {
 
-  public gano:boolean = false;
+  public gano:boolean    = false;
+  public text_tit:string = 'PERDISTE';
+  public text_des:string = 'SE NECESITABA CONTESTAR TODAS LAS PREGUNTAS BIEN';
 
   constructor(
     public preguntas: PreguntasService,
@@ -19,7 +21,10 @@ export class PantallaFinalComponent implements OnInit {
 
   ngOnInit() {
     this.gano = this.preguntas.gano_fase2 && this.preguntas.preguntaActual == 5;
-    
+    if (this.gano){
+      this.text_tit = 'FELICIDADES, ¡GANASTE!';
+      this.text_des = 'RETIRÁ TU PREMIO';
+    }
   }
 
   reset(){
